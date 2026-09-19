10e80554eb32967c0301d4563475ba8874c67f9c
Irbaz Abid <irbazabid58@gmail.com>
2026-09-19 09:13:29 +0500

    Changing of Flag

--- app.js
+++ app.js
@@ -5,9 +5,32 @@ for(let select of dropdowns){
         let newOption=document.createElement("option");
         newOption.innerText=currCode;
         newOption.value=currCode;
+        if(select.name=="from" && currCode=="USD"){
+            newOption.selected="selected";
+        }
+        else if(select.name=="To" &&     currCode=="INR"){
+            newOption.selected="selected";
+        }
         select.append(newOption);
     }
+
+    select.addEventListener("change",(evt)=>{
+        updateFlag(evt.target);
+    })
 }
+
+const updateFlag=(element)=>{
+      let currCode=element.value;
+      let CountryCode = countryList[currCode];
+      let newSrc=`https://flagsapi.com/${CountryCode}/flat/64.png`;
+      let image = element.parentElement.querySelector("img");
+
+      image.src=newSrc;
+}
+
+
+
+
 
 
 
