d75bf2a83246d96414833f884502e53c03a10b9e
Irbaz Abid <irbazabid58@gmail.com>
2026-09-19 08:41:47 +0500

    Country codes

---
+++ app.js
@@ -0,0 +1,13 @@
+let dropdowns=document.querySelectorAll(".dropdown select");
+
+for(let select of dropdowns){
+    for(let currCode in countryList){
+        let newOption=document.createElement("option");
+        newOption.innerText=currCode;
+        newOption.value=currCode;
+        select.append(newOption);
+    }
+}
+
+
+
